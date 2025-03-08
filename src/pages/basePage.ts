94ea6245905ecd0e04d45f96165b10f13e5ffe4c
Leonardo Carvalho <leo.leocarvalho@gmail.com>
2025-03-08 13:15:09 -0300

    add invalid credentials login test case, and classes improvements

--- src/pages/basePage.ts
+++ src/pages/basePage.ts
@@ -1,20 +1,36 @@
-import { Page } from '@playwright/test';
+import { Page, Locator, expect } from '@playwright/test';
 import { Config } from '../config';
 import { Texts } from '../helpers/texts';
-import { Assertions } from '../helpers/assertionsHelper';
 
 export class BasePage {
   config = new Config();
   texts = new Texts();
-  assertions = new Assertions();
-  
+
   protected page: Page;
   constructor(page: Page) {
     this.page = page;
   }
-  
+
   async goTo(url: string = this.config.baseURL) {
     await this.page.goto(url);
+  }
+
+  getLocator(selector: string): Locator {
+    return this.page.locator(selector);
+  }
+
+  async clickElement(selector: string) {
+    const element = this.getLocator(selector);
+    await element.click();
+  }
+
+  async assertElementText(element: string, expectedText: string) {
+      const locator = this.getLocator(element);
+      await expect(locator).toHaveText(expectedText);
+  }
+
+  async assertUrl(page: Page, expectedUrl: string) {
+      await expect(page).toHaveURL(expectedUrl);
   }
 
 }
